791a5aa541e2591f5ba46630a428ae92cd11120a
Leonardo Alves Pereira <leonardo.alves@tjdft.jus.br>
2020-09-09 15:57:54 -0300

    Fix instalation with python 3

--- ckanext/ldap/plugin.py
+++ ckanext/ldap/plugin.py
@@ -151,7 +151,7 @@ class LdapPlugin(SingletonPlugin):
 
             # make sure the config options are all unicode for LDAP
             if isinstance(toolkit.config.get(key, None), str):
-                toolkit.config[key] = unicode(toolkit.config.get(key))
+                toolkit.config[key] = toolkit.config.get(key)
         if len(errors):
             raise ConfigError(u'\n'.join(errors))
 
